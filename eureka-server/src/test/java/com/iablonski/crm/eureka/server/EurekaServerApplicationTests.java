06ec13207001729c1a1da58532d488380e9768e8
Alexander_I <alex.iablonski@gmail.com>
2024-03-18 23:06:19 +0100

    CRM system - Eureka server - Tests - v. 1.1.0

--- eureka-server/src/test/java/com/iablonski/crm/eureka/server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/iablonski/crm/eureka/server/EurekaServerApplicationTests.java
@@ -1,13 +1,59 @@
 package com.iablonski.crm.eureka.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
-class EurekaServerApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PlannerEurekaServerApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${eureka.client.register-with-eureka}")
+	private boolean registerWithEureka;
+
+	@Value("${eureka.client.fetch-registry}")
+	private boolean fetchRegistry;
+
+	@Value("${spring.cloud.config.uri}")
+	private String configServerUri;
 
 	@Test
 	void contextLoads() {
+		String health = restTemplate.getForObject("/actuator/health", String.class);
+		assertEquals("{\"status\":\"UP\"}", health);
 	}
 
+	@Test
+	void portConfigurationTest() {
+		assertEquals(8761, serverPort);
+	}
+
+	@Test
+	void applicationNameTest() {
+		assertEquals("eureka-server", applicationName);
+	}
+
+	@Test
+	void configServerUriTest() {
+		assertEquals("http://localhost:8888", configServerUri);
+	}
+
+	@Test
+	void eurekaConfigurationTest() {
+		assertFalse(registerWithEureka);
+		assertFalse(fetchRegistry);
+	}
 }
